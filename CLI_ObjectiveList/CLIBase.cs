0f6f12ef3a1dc869ac35aa977b8f4070ee882fd3
BelicusBr <X70r@outlook.com>
2023-02-17 21:48:54 -0300

    Adicionar arquivos de projeto.

---
+++ CLI_ObjectiveList/CLIBase.cs
@@ -0,0 +1,21 @@
+﻿using Cobilas.CLI.Manager;
+
+namespace Cobilas.CLI.ObjectiveList {
+    internal struct CLIBase {
+        public const string version = "1.0.0";
+        /*
+            root --version/-v
+            root --help/-h
+            root init/-i {arg:file path}
+            root --show/-s {arg:file path}
+            root add task --path/-p {opc:arg} --title/-t {opc:arg} --description/-d {opc:arg} {arg:file path}
+            root add block --path/-p {opc:arg} --title/-t {opc:arg} {arg:file path}
+         */
+        public static CLICommand Create()
+            => new CLICommand("root",
+                    new CLICommand(4,
+                            "--version/-v"
+                        )
+                );
+    }
+}
---
+++ CLI_ObjectiveList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Cobilas.CLI.Manager;
+
+namespace Cobilas.CLI.ObjectiveList {
+    class Program {
+        static void Main(string[] args) {
+            ErrorMensager error = new ErrorMensager();
+            CLIArgCollection collection = new CLIArgCollection();
+            CLICommand root = CLIBase.Create();
+
+            if (CLICommand.Cateter(new StringArrayToIEnumerator(args), root, collection, error, out int funcID)) {
+                if (funcID == 0)
+                    Console.WriteLine("CMD Unk [{0}]", JoinArgs(args));
+                else
+                    _ = FuncHub.Invok(funcID, error, collection);
+            } else {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                foreach (var item in error)
+                    Console.WriteLine(item);
+                Console.ResetColor();
+            }
+
+            collection.Clear();
+            root.Dispose();
+        }
+
+        static string JoinArgs(string[] args) {
+            StringBuilder builder = new StringBuilder();
+            for (int I = 0; I < args.Length; I++)
+                builder.AppendFormat("{0} ", args[I]);
+            return builder.ToString().TrimEnd();
+        }
+    }
+}
