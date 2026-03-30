31dc87610abcdda2d671d6126b81867b0816ece5
BelicusBr <x70r@outlook.com>
2026-03-30 16:34:32 -0300

    #3.1.0-ch.1

--- com.cobilas.cs.cli.objective-list/FuncHub/TaskListItem.cs
+++ com.cobilas.cs.cli.objective-list/FuncHub/TaskListItem.cs
@@ -1,46 +1,43 @@
 ﻿namespace Cobilas.CLI.ObjectiveList.FuncHub;
 
 internal readonly struct TaskListItem {
-	private readonly string path,
-							title,
+	private readonly string title,
 							description;
 	private readonly bool status;
-	private readonly int[] cells;
 	private readonly TaskPath taskPath;
 
-	internal int this[int index] => cells[index];
+	internal string Title => title;
+	internal bool Status => status;
+	internal string Path => taskPath.Path;
+	internal int[] Cells => taskPath.Cells;
+	internal TaskPath TaskPath => taskPath;
+	internal string Description => description;
+	internal int CellCount => taskPath.CellCount;
+
+	internal int this[int index] => taskPath[index];
 
 	internal TaskListItem(string path, string title, string description, string status) {
-		this.path = path;
 		this.title = title;
 		this.taskPath = new(path);
 		this.description = description;
 		this.status = bool.Parse(status);
-		this.cells = TaskPath.GetDimension(path);
 	}
 
 	internal TaskListItem(int path, string title, string description, string status) {
 		this.title = title;
-		this.cells = [path];
 		this.taskPath = new(path);
-		this.path = path.ToString();
 		this.description = description;
 		this.status = bool.Parse(status);
 	}
 
 	internal TaskListItem(int[] path, string title, string description, string status) {
-		this.cells = path;
 		this.title = title;
 		this.taskPath = new(path);
 		this.description = description;
 		this.status = bool.Parse(status);
-		this.path = string.Join('.', path);
 	}
 
-	internal string Path => path;
-	internal int[] Cells => cells;
-	internal string Title => title;
-	internal bool Status => status;
-	internal TaskPath TaskPath => taskPath;
-	internal string Description => description;
+	internal bool IsBlock(TaskListItem item) => taskPath.IsBlock(item.taskPath);
+
+	internal bool IsBlock(TaskPath item) => taskPath.IsBlock(item);
 }
