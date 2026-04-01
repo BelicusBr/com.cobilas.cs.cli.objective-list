3eabb9cad979b693129c67719aac23bf61a9c0c0
BelicusBr <x70r@outlook.com>
2026-04-01 19:50:52 -0300

    #3.2.0-d.ch.4

---
+++ project-git-funcs.ps1
@@ -0,0 +1,35 @@
+﻿$branchs = git branch
+
+$a_branch
+
+foreach ($branch in $branchs) {
+	if ($branch.Contains("*")) {
+		$a_branch = $branch.Trim("*").Trim()
+	}
+}
+
+. git-funcs
+
+Switch -Wildcard ($a_branch) {
+	"*.dev" {
+		merge "master.fix" $a_branch
+		merge "master" "master.fix"
+		git checkout $a_branch
+	}
+	"*.fix" {
+		merge "master.dev" $a_branch
+		merge "master" "master.dev"
+		git checkout $a_branch
+	}
+	default {
+		merge "master.dev" $a_branch
+		merge "master.fix" "master.dev"
+		git checkout $a_branch
+	}
+}
+
+$branchs = git remote
+
+foreach ($branch in $branchs) {
+	push-all $branch
+}
